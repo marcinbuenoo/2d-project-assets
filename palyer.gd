b42674c9b838c6fb4f4b5116a5c26983fb6d1170
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-24 18:04:17 -0300

    Correcting player character animation_24-01_18:04

--- palyer.gd
+++ palyer.gd
@@ -5,3 +5,8 @@ func _physics_process(delta):
 "move_up", "move_down")
 	velocity = direction * 600
 	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		get_node("HappyBoo").play_walk_animation()
+	else:
+		get_node("HappyBoo").play_idle_animation()
